4300bffb9e9bda4e958aa1a7e00e520ac4356c2e
Adam <adam@example.com>
2025-10-12 13:39:24 -0700

    Finish rendering map

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from object_renderer import *
 class Game:
     def __init__ (self):
         pg.init()
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
@@ -28,14 +29,14 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
         
     def draw(self):
-        self.screen.fill('black')
+        #self.screen.fill('black')
         self.object_renderer.draw()
         # self.map.draw()
         # self.player.draw()
         
     def check_events(self):
         for event in pg.event.get():
-            if event.type == pg.QUIT:
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
         
