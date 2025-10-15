b02fea390c99d06852a0ab81edb488463923297f
Adam <adam@example.com>
2025-10-14 21:33:57 -0700

    fix walls

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
         
     def draw(self):
+        # self.screen.fill('black')
         self.object_renderer.draw()
         self.weapon.draw()
         # self.map.draw()
