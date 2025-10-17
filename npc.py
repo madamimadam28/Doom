84eb4d279928b0d175f94384e16a9f186c679b38
Adam <adam@example.com>
2025-10-16 22:08:16 -0700

    Complete barebones game

--- npc.py
+++ npc.py
@@ -21,12 +21,39 @@ class NPC(AnimatedSprite):
         self.pain = False
         self.ray_cast_value = False
         self.frame_counter = 0
+        self.player_search_trigger = False
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
         self.run_logic()
         # self.draw_ray_cast()
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx * self.size), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * self.size)):
+            self.y += dy
+
+    def movement(self):
+        next_pos = self.game.pathfinding.get_path(self.map_pos, self.game.player.map_pos)
+        next_x, next_y = next_pos
+
+        # pg.draw.rect(self.game.screen, 'blue', (next_x * 100, next_y * 100, 100, 100))
+        if next_pos not in self.game.object_handler.npc_positions:
+            angle = math.atan2(next_y + 0.5 - self.y, next_x + 0.5 - self.x)
+            dx = math.cos(angle) * self.speed
+            dy = math.sin(angle) * self.speed
+            self.check_wall_collision(dx, dy)
+
+    def attack(self):
+        if self.animation_trigger:
+            self.game.sound.npc_shot.play()
+            if random() < self.accuracy:
+                self.game.player.get_damage(self.attack_damage)
 
     def animate_death(self):
         if not self.alive:
@@ -60,6 +87,21 @@ class NPC(AnimatedSprite):
             self.check_hit_in_npc()
             if self.pain:
                 self.animate_pain()
+
+            elif self.ray_cast_value:
+                self.player_search_trigger = True
+
+                if self.dist < self.attack_dist:
+                    self.animate(self.attack_images)
+                    self.attack()
+                else:
+                    self.animate(self.walk_images)
+                    self.movement()
+
+            elif self.player_search_trigger:
+                self.animate(self.walk_images)
+                self.movement()
+
             else:
                 self.animate(self.idle_images)
         else:
